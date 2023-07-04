d6a2f71a87cad0056c5f0be795bcb151e48b2531
SwethaMuthu123 <138576735+SwethaMuthu123@users.noreply.github.com>
2023-07-04 21:01:15 +0530

    Java file created

---
+++ Employee.java
@@ -0,0 +1,58 @@
+package com.accenture.inheritance;
+
+public class Employee extends Person{
+	private int empId;
+	private double salary;
+	private int performancerating;
+	
+	public Employee()
+	{
+		super();
+		empId=34567;
+		salary=45000;
+		performancerating=3;
+	}
+	
+	public Employee(int empId,double salary,int performancerating)
+	{
+		super("preethi","lakshmi",56);
+		this.empId=empId;
+		this.salary=salary;
+		this.performancerating=performancerating;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public int getPerformancerating() {
+		return performancerating;
+	}
+
+	public void setPerformancerating(int performancerating) {
+		this.performancerating = performancerating;
+	}
+	
+	public void printEmployeeDetails()
+	{
+		System.out.println("FirstName:"+getFirstName());
+		System.out.println("LastName:"+getLastName());
+		System.out.println("age:"+getAge());
+		System.out.println("Empid:"+getEmpId());
+		System.out.println("Salary:"+getSalary());
+		System.out.println("Performance Rating:"+getPerformancerating());
+		
+	}
+}
